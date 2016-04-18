3ea6596db839331e63e5cbc89a1c20750d350c2c
Ken Dreyer <kdreyer@redhat.com>
2016-04-18 09:16:50 -0600

    make "python setup.py test" work

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 import os
 import re
+import sys
+from setuptools.command.test import test as TestCommand
 
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 LONG_DESCRIPTION = open(readme).read()
 
 module_file = open("rhcephpkg/__init__.py").read()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main('rhcephpkg/tests ' + self.pytest_args)
+        sys.exit(errno)
 
 
 from setuptools import setup
@@ -31,5 +51,6 @@ setup(
     tests_require    = [
         'pytest',
         'httpretty',
-    ]
+    ],
+    cmdclass = {'test': PyTest},
 )
