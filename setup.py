c06987aa33bf83103622dbfe7d0fdefccf9d435f
Ken Dreyer <kdreyer@redhat.com>
2017-01-12 17:39:06 -0700

    setup.py: add bump command

--- setup.py
+++ setup.py
@@ -4,13 +4,58 @@ import subprocess
 import sys
 from setuptools.command.test import test as TestCommand
 from setuptools import Command
+try:
+    # Python 2 backwards compat
+    from __builtin__ import raw_input as input
+except ImportError:
+    pass
 
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 LONG_DESCRIPTION = open(readme).read()
 
-module_file = open("rhcephpkg/__init__.py").read()
+
+def read_module_contents():
+    with open('rhcephpkg/__init__.py') as app_init:
+        return app_init.read()
+
+
+module_file = read_module_contents()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
 version = metadata['version']
+
+
+class BumpCommand(Command):
+    """ Bump the __version__ number and commit all changes. """
+
+    user_options = [('version=', 'v', 'version number to use')]
+
+    def initialize_options(self):
+        new_version = metadata['version'].split('.')
+        new_version[-1] = str(int(new_version[-1]) + 1)  # Bump the final part
+        self.version = ".".join(new_version)
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        print('old version: %s  new version: %s' %
+              (metadata['version'], self.version))
+        try:
+            input('Press enter to confirm, or ctrl-c to exit >')
+        except KeyboardInterrupt:
+            raise SystemExit("\nNot proceeding")
+
+        old = "__version__ = '%s'" % metadata['version']
+        new = "__version__ = '%s'" % self.version
+        module_file = read_module_contents()
+        with open('rhcephpkg/__init__.py', 'w') as fileh:
+            fileh.write(module_file.replace(old, new))
+
+        # Commit everything with a standard commit message
+        cmd = ['git', 'commit', '-a', '-m', 'version %s' % self.version]
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
 
 
 class ReleaseCommand(Command):
@@ -89,5 +134,5 @@ setup(
         'pytest-flake8',
         'httpretty',
     ],
-    cmdclass={'test': PyTest, 'release': ReleaseCommand},
+    cmdclass={'test': PyTest, 'bump': BumpCommand, 'release': ReleaseCommand},
 )
