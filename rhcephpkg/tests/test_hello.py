251184776e79444462cd1eb9684117c93319e56d
Ken Dreyer <kdreyer@redhat.com>
2017-05-23 11:27:11 -0600

    tests: rm httpretty from test_hello.py
    Remove the dependency on httpretty from test_hello.py, and fake six's
    urlopen method instead. Introduce the tests.util module along with
    the static fixture file layout.

--- rhcephpkg/tests/test_hello.py
+++ rhcephpkg/tests/test_hello.py
@@ -1,12 +1,7 @@
 import os
-import httpretty
 from rhcephpkg import Hello
+from rhcephpkg.tests.util import fake_urlopen
 
-try:
-    from jenkins import CRUMB_URL
-except ImportError:
-    # CRUMB_URL was introduced in python-jenkins v0.2.2
-    CRUMB_URL = 'crumbIssuer/api/json'
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
@@ -14,32 +9,15 @@ FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
 
 class TestHelloJenkins(object):
 
-    @classmethod
-    def setup_class(cls):
-        httpretty.enable()
-        httpretty.HTTPretty.allow_net_connect = False
-
-    @classmethod
-    def teardown_class(cls):
-        httpretty.disable()
-
     def test_success(self, monkeypatch, capsys):
         monkeypatch.setenv('HOME', FIXTURES_DIR)
-        # "Who Am I" endpoint.
-        httpretty.register_uri(httpretty.GET,
-                               'https://ceph-jenkins.example.com/me/api/json',
-                               body='{"fullName": "Ken"}',
-                               content_type='text/json')
-        # python-jenkins v0.2.2+ checks this CRUMB_URL in its
-        # maybe_add_crumb(). Return a 404 so python-jenkins catches the
-        # NotFoundException and carries on.
-        httpretty.register_uri(httpretty.GET,
-                               'https://ceph-jenkins.example.com/' + CRUMB_URL,
-                               status=404)
-        # "Jenkins Version" endpoint.
-        httpretty.register_uri(httpretty.GET,
-                               'https://ceph-jenkins.example.com/',
-                               adding_headers={'X-Jenkins': '1.5'})
+        # python-jenkins uses a "from" import, "from X import Y", so
+        # monkeypatching is trickier.
+        monkeypatch.setattr('jenkins.urlopen', fake_urlopen)
+        # Another option would be to use func_code. This globally patches all
+        # calls to urlopen(), not just jenkins' calls.
+        # monkeypatch.setattr('six.moves.urllib.request.urlopen.func_code',
+        #                     fake_urlopen.func_code)
         hello = Hello(())
         hello._run()
         out, _ = capsys.readouterr()
---
+++ rhcephpkg/tests/util.py
@@ -0,0 +1,50 @@
+import errno
+from io import StringIO
+import os
+import six
+from six.moves.urllib.parse import urlparse
+from six.moves.http_client import HTTPMessage
+from six.moves.urllib.error import HTTPError
+from six.moves.urllib.request import urlopen
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
+
+
+def fake_urlopen(req, **kw):
+    """
+    Behave like six.moves.urllib.request.urlopen().
+
+    Return the contents of local fixture files on disk instead.
+    """
+    o = urlparse(req.get_full_url())
+    localfile = os.path.join(FIXTURES_DIR, o.netloc, o.path[1:])
+    # Try reading the file, and handle some special cases if we get an error.
+    try:
+        with open(localfile):
+            pass
+    except IOError as e:
+        # Raise HTTP 404 errors for non-existent files.
+        if e.errno == errno.ENOENT:
+            url = req.get_full_url()
+            headers = HTTPMessage(StringIO(u''))
+            raise HTTPError(url, 404, 'Not Found', headers, None)
+        # If URL looked like a directory ("/"), open the file instead.
+        elif e.errno == errno.ENOTDIR:
+            localfile = localfile.rstrip('/')
+        # If localfile's a directory, look for a matching ".body" file instead.
+        elif e.errno == errno.EISDIR:
+            localfile = localfile.rstrip('/') + '.body'
+        else:
+            raise
+
+    response = urlopen('file://' + localfile)
+
+    # Inject X-Jenkins response header here, if needed.
+    # ...hacks ahead
+    if six.PY2 and 'jenkins' in o.netloc:
+        response.headers = HTTPMessage(StringIO(u'X-Jenkins: 1.5'))
+    if six.PY3 and 'jenkins' in o.netloc:
+        response.getheader = lambda x: '1.5' if x == 'X-Jenkins' else None
+
+    return response
