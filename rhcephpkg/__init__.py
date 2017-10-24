afc4448b66225abad5c76d08ce52c1047a06d5a2
Ken Dreyer <kdreyer@redhat.com>
2017-10-24 10:04:19 -0600

    version 1.7.1

--- rhcephpkg/__init__.py
+++ rhcephpkg/__init__.py
@@ -15,4 +15,4 @@ __all__ = ['log', 'Build', 'CheckoutFromPatches', 'Clone', 'Download',
            'Gitbz', 'Hello', 'Localbuild', 'MergePatches', 'Patch', 'Source',
            'WatchBuild']
 
-__version__ = '1.7.0'
+__version__ = '1.7.1'
