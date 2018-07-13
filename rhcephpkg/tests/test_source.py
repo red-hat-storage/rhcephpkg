91cc2457a1dad9b8517fcbed486518a360cc4d1e
Ken Dreyer <kdreyer@redhat.com>
2018-07-13 11:45:17 -0600

    tests: test "source --help"
    Add a test for the "source --help" sub-command behavior.

--- rhcephpkg/tests/test_source.py
+++ rhcephpkg/tests/test_source.py
@@ -1,8 +1,16 @@
+import pytest
 from rhcephpkg import Source
 from rhcephpkg.tests.util import CallRecorder
 
 
 class TestSource(object):
+
+    def test_help(self, capsys):
+        source = Source(['rhcephpkg', 'source', '--help'])
+        with pytest.raises(SystemExit):
+            source.main()
+        out, _ = capsys.readouterr()
+        assert "Build a source package on the local system." in out
 
     def test_source(self, monkeypatch):
         recorder = CallRecorder()
